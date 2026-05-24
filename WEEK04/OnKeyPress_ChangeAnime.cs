4022f03642ec7f3d2150b976ec68d5d9d45ca5c0
jiwooya0123 <jiwooya0123@gmail.com>
2026-05-24 22:45:32 +0900

    4주차 과제

---
+++ WEEK04/OnKeyPress_ChangeAnime.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKeyPress_ChangeAnime : MonoBehaviour
+{
+    public string upAnime = "";
+    public string downAnime = "";
+    public string rightAnime = ""; 
+    public string leftAnime = "";
+
+    string nowMode = "";
+
+    void Start()
+    {
+        nowMode = downAnime;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("up"))
+        {
+            nowMode = upAnime;
+        }
+        if(Input.GetKey("down"))
+        {
+            nowMode = downAnime;
+        }
+        if(Input.GetKey("right"))
+        {
+            nowMode = rightAnime;
+        }
+        if(Input.GetKey("left"))
+        {
+            nowMode = leftAnime;
+        }
+    }
+    void FixedUpdate()
+    {
+        this.GetComponent<Animator>().Play(nowMode);
+    }
+}
